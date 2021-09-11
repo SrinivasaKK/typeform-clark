53b1a74712ce22d660897a847fc55763cc768e5d
srini_947 <srinivasa.keshavamurthy@chubb.com>
2021-09-12 01:29:22 +0800

    added enter key event listener

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -14,5 +14,24 @@ export class WelcomeComponent implements OnInit {
 
   emitStartButtonClickEvent() {
     this.clickStart.emit();
+    console.log('here');
+  }
+
+  ngAfterViewInit() {
+    // https://stackoverflow.com/questions/38223945/window-removeeventlistener-with-a-named-function-isnt-working
+    // need to same reference in order to remove the event listener
+    this.enterKeyEvent = this.enterKeyEvent.bind(this);
+
+    // add event listener to enter key to trigger the questionnaire
+    document.addEventListener('keyup', this.enterKeyEvent);
+  }
+
+  enterKeyEvent() {
+    this.emitStartButtonClickEvent();
+    this.removeEventListener();
+  }
+
+  removeEventListener() {
+    document.removeEventListener('keyup', this.enterKeyEvent);
   }
 }
